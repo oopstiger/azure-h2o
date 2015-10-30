847b14d94fd40c9af3250bd0c796a0009fd9679f
Riz <riz.hsu@gmail.com>
2015-10-30 19:51:36 +0800

    setup azure env

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const koa = require('koa');
+const bodyParser = require('koa-body-parser');
 
 let app = koa();
+app.use(bodyParser());
+
 let port = process.env.PORT || 1337;
 
 app.use(function *(){
-  this.body = {
-    message: "ok",
-    data: 2728911,
-    code: 0
-  }
+  this.body = process.env;
 });
 
 app.listen(port);
